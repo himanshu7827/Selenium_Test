3724d0cb1010cbacb2f0b1b61d7151714c092473
Himanshu Pandey <123876793+himanshu7827@users.noreply.github.com>
2024-07-03 17:16:44 +0530

    MSGG

--- src/test/java/com/selenium/Tests/OrangeHRM.java
+++ src/test/java/com/selenium/Tests/OrangeHRM.java
@@ -18,36 +18,37 @@ public class OrangeHRM {
 		driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
-	}
-	
-	@Test
-	public void EnterLoginDetails() {
-		WebDriverManager.edgedriver().setup();
-		driver = new EdgeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		//driver.findElement(By.name("username")).sendKeys("Admin");
-		//driver.findElement(By.name("password")).sendKeys("admin123");
-		driver.findElement(By.xpath("//button[text()=' Login ']")).click();
-	}
-	
-	@Test
-	public void NavigateToMyInfo() {
-		driver.findElement(By.xpath("//p[@class=\'oxd-icon oxd-main-menu-item--icon\']")).click();
-	}
-	
-	@Test
-	public void VerifyMyInfo() {
-		System.out.println(driver.findElement(By.id("employee-details")).isDisplayed());
 		driver.quit();
 	}
 	
-	@Test
-	public void VerifyLogin() {
-		WebElement element = driver.findElement(By.xpath("//p[@class='oxd-userdropdown-name' and text()='manda user']"));
-		System.out.println(element.isDisplayed());
-		System.out.println(element.getText());
-		driver.quit();
-	}
+	// @Test
+	// public void EnterLoginDetails() {
+	// 	WebDriverManager.edgedriver().setup();
+	// 	driver = new EdgeDriver();
+	// 	driver.manage().window().maximize();
+	// 	driver.get("https://opensource-demo.orangehrmlive.com/");
+	// 	//driver.findElement(By.name("username")).sendKeys("Admin");
+	// 	//driver.findElement(By.name("password")).sendKeys("admin123");
+	// 	driver.findElement(By.xpath("//button[text()=' Login ']")).click();
+	// }
+	
+	// @Test
+	// public void NavigateToMyInfo() {
+	// 	driver.findElement(By.xpath("//p[@class=\'oxd-icon oxd-main-menu-item--icon\']")).click();
+	// }
+	
+	// @Test
+	// public void VerifyMyInfo() {
+	// 	System.out.println(driver.findElement(By.id("employee-details")).isDisplayed());
+	// 	driver.quit();
+	// }
+	
+	// @Test
+	// public void VerifyLogin() {
+	// 	WebElement element = driver.findElement(By.xpath("//p[@class='oxd-userdropdown-name' and text()='manda user']"));
+	// 	System.out.println(element.isDisplayed());
+	// 	System.out.println(element.getText());
+	// 	driver.quit();
+	// }
 
 }
